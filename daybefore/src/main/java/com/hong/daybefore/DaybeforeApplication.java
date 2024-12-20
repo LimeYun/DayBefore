43310132ac0200a058ae7f79b78ef738409b4e77
LimeYun <jstar3183@naver.com>
2024-12-20 18:16:09 +0900

    프로젝트 생성

---
+++ daybefore/src/main/java/com/hong/daybefore/DaybeforeApplication.java
@@ -0,0 +1,13 @@
+package com.hong.daybefore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybeforeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaybeforeApplication.class, args);
+	}
+
+}
---
+++ daybefore/src/test/java/com/hong/daybefore/DaybeforeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.daybefore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaybeforeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
